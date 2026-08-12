7227d811af8248dcf95d069a5e18aea848d246b8
Gokalp <gokalppabdullah@gmail.com>
2026-08-12 15:28:50 +0300

    Notes API and MVC

---
+++ MinimalNoteApp/Datas/NoteDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalNoteApp.Models;
+
+namespace MinimalNoteApp.Datas
+{
+    public class NoteDbContext :DbContext
+    {
+        public NoteDbContext(DbContextOptions<NoteDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Note> Notes { get; set; }
+    }
+}
--- MinimalNoteApp/Program.cs
+++ MinimalNoteApp/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalNoteApp.Datas;
 using MinimalNoteApp.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,51 +8,63 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+
+builder.Services.AddDbContext<NoteDbContext>(options =>
+    options.UseSqlServer
+    (builder.Configuration.GetConnectionString
+    ("DefaultConnection"))
+);
+
 var app = builder.Build();
 
-var notes = new List<Note>();
-var nextId = 1;
-
 //Create
-app.MapPost("/notes", (Note not) =>
+app.MapPost("/notes", async (Note not, NoteDbContext db) =>
 {
-    not.Id = nextId++;
-    not.CreatedAt = DateTime.Now;
-    notes.Add(not);
-
-    return Results.Created($"/notes/{not.Id}", not); // burada Results.Created ile 201 Created status code dönüyoruz ve yeni oluşturulan notun URL'sini döndürüyoruz
+    not.CreatedAt = DateTime.Now; //burada not objesinin CreatedAt özelliğini şu anki tarih ve saat ile ayarlıyoruz
+    db.Notes.Add(not); //   burada db nesnesinin Notes özelliğine not objesini ekliyoruz
+    await db.SaveChangesAsync();
+    return Results.Ok("Not Oluşturuldu");
 });
 
 //Read all
-app.MapGet("/notes", () =>
+app.MapGet("/notes", async (NoteDbContext db) =>
 {
+    var notes = await db.Notes.ToListAsync(); //burada db nesnesinin Notes özelliğini kullanarak veritabanındaki tüm notları liste halinde alıyoruz
     return Results.Ok(notes);
 });
 
 //Read by id
-app.MapGet("/notes/{id:int}", (int id) =>
+app.MapGet("/notes/{id:int}", async (int id, NoteDbContext db) =>
 {
-    var note = notes.FirstOrDefault(n => n.Id == id);// burada FirstOrDefault ile id'si verilen notu buluyoruz
-    return note is not null ? Results.Ok
-    (note) : Results.NotFound(); // eğer not bulunursa 200 OK dönüyoruz, bulunmazsa 404 Not Found dönüyoruz
+    var note = await db.Notes.FindAsync(id); //burada db nesnesinin Notes özelliğini kullanarak veritabanındaki belirli bir notu id ile buluyoruz
+    if(note == null)
+    {
+        return Results.NotFound(); //eğer not bulunamazsa NotFound() metodunu çağırıyoruz
+    }
+    return Results.Ok(note);
 });
 
 //Update
-app.MapPut("/notes/{id:int}", (int id, Note updatedNote) =>
+app.MapPut("/notes/{id:int}", async (int id, Note updatedNote, NoteDbContext db) =>
 {
-    var note = notes.FirstOrDefault(n => n.Id == id);// FirstOrDefault ile id'si verilen notu buluyoruz
-    if (note is null)
-    {
-        return Results.NotFound();// not bulunmazsa 404 Not Found dönüyoruz
-    }
-
-    note.Title = updatedNote.Title;//  notun başlığını güncelliyoruz
-    note.Content = updatedNote.Content;//  notun içeriğini güncelliyoruz
-
-    return Results.Ok(note);
+    var note = await db.Notes.FindAsync(id);
+    if (note == null) return Results.NotFound();
+    note.Title = updatedNote.Title;
+    note.Content = updatedNote.Content;
+    await db.SaveChangesAsync();
+    return Results.Ok("Not Güncellendi");
 
 });
 
+//Delete
+app.MapDelete("/notes/{id:int}", async (int id, NoteDbContext db) =>
+{
+    var note = await db.Notes.FindAsync(id);
+    if(note == null) return Results.NotFound();
+    db.Notes.Remove(note);
+    await db.SaveChangesAsync();
+    return Results.Ok("Not Silindi");
+});
 
 
 
