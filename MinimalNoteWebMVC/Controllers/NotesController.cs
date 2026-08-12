766dce4139e952823c726d2ca5351c93be014be1
Gokalp <gokalppabdullah@gmail.com>
2026-08-12 15:36:49 +0300

    Notes API and MVC

---
+++ MinimalNoteWebMVC/Controllers/NotesController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MinimalNoteWebMVC.Models;
+using System.Text;
+using System.Text.Json;
+
+namespace MinimalNoteWebMVC.Controllers
+{
+    public class NotesController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public NotesController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var response = await client.GetAsync("notes");
+            if(!response.IsSuccessStatusCode) 
+            {            
+                return View(new List<Note>());
+            }
+            var notes = await
+                response.Content.ReadFromJsonAsync<List<Note>>();
+
+            return View(notes);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Note note)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+
+            var content = new StringContent(JsonSerializer.Serialize(note), Encoding.UTF8, "application/json");
+
+            var response = await client.PostAsync("notes", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            ModelState.AddModelError(string.Empty, "Bir Hata Oluştu");
+            return View(note);
+        }
+
+        public async Task<IActionResult> Detail(int id) 
+        { 
+            var client = _httpClientFactory.CreateClient("NotesApi");// oluşturduğumuz client'ı kullanarak apiye istek atıyoruz
+            var response = await client.GetAsync($"notes/{id}");// apiye istek atıyoruz ve id'yi parametre olarak gönderiyoruz
+            if (!response.IsSuccessStatusCode)
+            {
+                return View(new Note());
+            }
+            var note = await response.Content.ReadFromJsonAsync<Note>();
+            return View(note);
+
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var response = await client.DeleteAsync($"notes/{id}");
+            //if (!response.IsSuccessStatusCode)
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+            var response = await client.GetAsync($"notes/{id}");
+            if (!response.IsSuccessStatusCode)
+            {
+                return NotFound();
+            }
+            var note = await response.Content.ReadFromJsonAsync<Note>(); 
+            
+            return View(note);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Note note)
+        {
+            var client = _httpClientFactory.CreateClient("NotesApi");
+
+            var content = new StringContent(JsonSerializer.Serialize(note), Encoding.UTF8, "application/json");
+
+            var response = await client.PutAsJsonAsync($"notes/{id}", note);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            ModelState.AddModelError(string.Empty, "Bir Hata Oluştu");
+            return View(note);
+        }
+    }
+}
